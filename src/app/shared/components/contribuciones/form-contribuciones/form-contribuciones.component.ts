4a837e6c1f81211c817d0e9619141deac4b6e511
battysauiro <battyguide8428@gmail.com>
2022-07-15 09:00:17 -0500

    se acabo el modulo de actualizar de derechos licencias

--- src/app/shared/components/contribuciones/form-contribuciones/form-contribuciones.component.ts
+++ src/app/shared/components/contribuciones/form-contribuciones/form-contribuciones.component.ts
@@ -1,8 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Catalogo } from 'src/app/shared/modelo/contribuciones/catalogo';
+import { ContribucionDerechosGenerales } from 'src/app/shared/modelo/contribuciones/contribucion-derechos-generales';
+import { ContribucionDerechosLicencias } from 'src/app/shared/modelo/contribuciones/contribucion-derechos-licencias';
 import { ContribucionImpuestos } from 'src/app/shared/modelo/contribuciones/contribucion-impuestos';
+import { Periodicidad } from 'src/app/shared/modelo/contribuciones/periodicidad';
 import { TipoPago } from 'src/app/shared/modelo/contribuciones/tipo-pago';
+import { ContribucionDerechosGService } from 'src/app/shared/servicio/contribuciones/contribucion-derechos-g.service';
+import { ContribucionDerechosLicenciasService } from 'src/app/shared/servicio/contribuciones/contribucion-derechos-licencias.service';
 import { ContribucionImpuestoService } from 'src/app/shared/servicio/contribuciones/contribucion-impuesto.service';
 import { ContribucionesService } from 'src/app/shared/servicio/contribuciones/contribuciones.service';
 import swal from 'sweetalert2';
@@ -15,22 +20,29 @@ import swal from 'sweetalert2';
 export class FormContribucionesComponent implements OnInit {
 
   contribucionImpuesto= new ContribucionImpuestos();
+  contribucionDerechosG= new ContribucionDerechosGenerales();
+  contribucionDerechosL= new ContribucionDerechosLicencias();
   tipoPagos:TipoPago[];
   descripciones:Catalogo[];
+  tDerechos:Catalogo[];
   tImpuestos:Catalogo[];
+  periodicidades:Periodicidad[];
   idFound=false;
   tipoContribucion:number=0;//aqui decidira cual vista mostrar
   constructor(
     public contribucionesService:ContribucionesService,
     public router:Router,
     public activatedRouter:ActivatedRoute,
-    public contribucionImpuestoService:ContribucionImpuestoService
+    public contribucionImpuestoService:ContribucionImpuestoService,
+    public contribucionDerechosGService:ContribucionDerechosGService,
+    public contribucionDLicenciasService:ContribucionDerechosLicenciasService
   ) { }
 
   ngOnInit(): void {
     this.cargarContribucion();
     this.obtenerTipoPago();
     this.obtenerDescripciones();
+    this.obtenerPeriodicidad();
 
   }
 
@@ -44,6 +56,16 @@ export class FormContribucionesComponent implements OnInit {
         this.obtenerTipoImpuesto();
         this.idFound=true;
         this.contribucionImpuestoService.ObtenerCImpuesto(id).subscribe(contribucion=>this.contribucionImpuesto=contribucion)
+      }
+      if(tipo==2){
+        this.obtenerTipoDerecho();
+        this.idFound=true;
+        this.contribucionDerechosGService.obtenerCDerechoG(id).subscribe(contribucion=>this.contribucionDerechosG=contribucion)
+      }
+      if(tipo==3){
+        this.obtenerTipoDerecho();
+        this.idFound=true;
+        this.contribucionDLicenciasService.ObtenerCDerechosLicencias(id).subscribe(contribucion=>this.contribucionDerechosL=contribucion)
       }
     });
   }
@@ -72,7 +94,48 @@ export class FormContribucionesComponent implements OnInit {
       }
     );
   }
+  /*------------------------------------------------------------------------*/
+  //funciones de la contribucion derechos generales
+  public crearDerechoG():void{
+    this.contribucionDerechosGService.crearCDerechoG(this.contribucionDerechosG).subscribe(
+      response=> {this.contribucionDerechosG=response;
+                  this.irDerechosG();
+                  swal('Derecho General Agregado',`Derecho General ${this.contribucionDerechosG.codigo_contribucion} añadido con éxito`,'success');
+                }
+    );
+  }
 
+  public actualizarDerechosG():void{
+    this.contribucionDerechosGService.actualizarCDerechoG(this.contribucionDerechosG).subscribe(contribucion=>{
+      this.irDerechosG();
+      swal('Derecho General Actualizado',`Derecho General ${contribucion.codigo_contribucion} actualizado con éxito`,'success');
+    });
+  }
+
+  obtenerTipoDerecho(){
+    this.contribucionesService.obtenerCatalogoDerechos().subscribe(
+      response=> {this.tDerechos= response
+      }
+    );
+  }
+  /*--------------------------------------------------------------*/
+  //funciones de la contribucion derechos licencias
+  public crearDerechosL():void{
+    this.contribucionDLicenciasService.crearCDerechosLicencias(this.contribucionDerechosL).subscribe(
+      response=> {this.contribucionDerechosL=response;
+                  this.irDerechosL();
+                  swal('Derecho Licencia Agregado',`Derecho Licencia ${this.contribucionDerechosL.codigo_contribucion} añadido con éxito`,'success');
+                }
+    );
+  }
+
+  public actualizarDerechosL():void{
+    this.contribucionDLicenciasService.actualizarCDerechosLicencias(this.contribucionDerechosL).subscribe(contribucion=>{
+      this.irDerechosL();
+      swal('Derecho Licencia Actualizado',`Derecho Licencia ${contribucion.codigo_contribucion} actualizado con éxito`,'success');
+    });
+  }
+  /*--------------------------------------------------------------*/
   //funciones de metodos generales
   obtenerTipoPago(){
     this.contribucionesService.obtenerTipoPago().subscribe(
@@ -89,6 +152,33 @@ export class FormContribucionesComponent implements OnInit {
     );
   }
 
+  obtenerPeriodicidad(){
+    this.contribucionesService.obtenerPeriodicidad().subscribe(
+      response=> {this.periodicidades= response
+      }
+    );
+  }
+
+  compararPago(o1:TipoPago,o2:TipoPago){
+    if(o1===undefined && o2===undefined){
+      return true;
+    }
+    return o1===null || o2===null || o1===undefined || o2===undefined? false:o1.id_pago===o2.id_pago;
+  }
+
+  compararDescripcion(o1:Catalogo,o2:Catalogo){
+    if(o1===undefined && o2===undefined){
+      return true;
+    }
+    return o1===null || o2===null || o1===undefined || o2===undefined? false:o1.id_catalogo===o2.id_catalogo;
+  }
+
+  compararPeriodicidad(o1:Periodicidad,o2:Periodicidad){
+    if(o1===undefined && o2===undefined){
+      return true;
+    }
+    return o1===null || o2===null || o1===undefined || o2===undefined? false:o1.id_periodicidad===o2.id_periodicidad;
+  }
 
   //reenvio de rutas
   irImpuestos(){
@@ -118,21 +208,7 @@ export class FormContribucionesComponent implements OnInit {
   irOtrosProductos(){
     this.router.navigate(['/otrosProductos']);
   }
-
-  compararPago(o1:TipoPago,o2:TipoPago){
-    if(o1===undefined && o2===undefined){
-      return true;
-    }
-    return o1===null || o2===null || o1===undefined || o2===undefined? false:o1.id_pago===o2.id_pago;
-  }
-
-  compararDescripcion(o1:Catalogo,o2:Catalogo){
-    if(o1===undefined && o2===undefined){
-      return true;
-    }
-    return o1===null || o2===null || o1===undefined || o2===undefined? false:o1.id_catalogo===o2.id_catalogo;
-  }
-
+  //comparadores de las contribuciones
   compararTImpuesto(o1:Catalogo,o2:Catalogo){
     if(o1===undefined && o2===undefined){
       return true;
@@ -140,12 +216,47 @@ export class FormContribucionesComponent implements OnInit {
     return o1===null || o2===null || o1===undefined || o2===undefined? false:o1.id_catalogo===o2.id_catalogo;
   }
 
-  public vacio(){
+  compararTDerecho(o1:Catalogo,o2:Catalogo){
+    if(o1===undefined && o2===undefined){
+      return true;
+    }
+    return o1===null || o2===null || o1===undefined || o2===undefined? false:o1.id_catalogo===o2.id_catalogo;
+  }
+
+  //validacion para los campos de cada contribucion
+  public vacioImpuesto(){
     if(this.contribucionImpuesto.codigo_contribucion==null || this.contribucionImpuesto.codigo_contribucion=="" ||
       this.contribucionImpuesto.concepto_contribucion==null || this.contribucionImpuesto.concepto_contribucion=="" ||
       this.contribucionImpuesto.id_tipo_pago==null ||
       this.contribucionImpuesto.id_descripcion==null ||
       this.contribucionImpuesto.catalogo_impuesto==null){
+        return true;
+      }
+        else{
+          return false;
+        }
+  }
+
+  public vacioDerechosG(){
+    if(this.contribucionDerechosG.codigo_contribucion==null || this.contribucionDerechosG.codigo_contribucion=="" ||
+      this.contribucionDerechosG.concepto_contribucion==null || this.contribucionDerechosG.concepto_contribucion=="" ||
+      this.contribucionDerechosG.id_tipo_pago==null ||
+      this.contribucionDerechosG.id_descripcion==null ||
+      this.contribucionDerechosG.catalogo_derechos==null ||
+      this.contribucionDerechosG.id_periodicidad==null){
+        return true;
+      }
+        else{
+          return false;
+        }
+  }
+
+  public vacioDerechosL(){
+    if(this.contribucionDerechosL.codigo_contribucion==null || this.contribucionDerechosL.codigo_contribucion=="" ||
+      this.contribucionDerechosL.concepto_contribucion==null || this.contribucionDerechosL.concepto_contribucion=="" ||
+      this.contribucionDerechosL.id_tipo_pago==null ||
+      this.contribucionDerechosL.id_descripcion==null ||
+      this.contribucionDerechosL.catalogo_derechos==null){
         return true;
       }
         else{
